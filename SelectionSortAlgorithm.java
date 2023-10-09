980b0354e127f016b80670a684a8ddb69fad311f
Nathaniel Mclaughlin <Nmclaughlin296@gmail.com>
2023-10-09 18:54:18 +0000

    Selection Sort Algorithm

---
+++ SelectionSortAlgorithm.java
@@ -0,0 +1,40 @@
+public class SelectionSortAlgorithm {
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("\nSorted Array:");
+        printArray(arr);
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted array
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}

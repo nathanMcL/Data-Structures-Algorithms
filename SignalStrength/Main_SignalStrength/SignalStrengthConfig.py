0bce5cf7085229595243e809909bdd14123303ea
Nathaniel Mclaughlin <Nmclaughlin296@gmail.com>
2023-12-06 18:52:02 +0000

    updated SignalStrength and Test source code

--- SignalStrength/Main_SignalStrength/SignalStrengthConfig.py
+++ SignalStrength/Main_SignalStrength/SignalStrengthConfig.py
@@ -1,2 +1,2 @@
 # Path to the CSV file
-csv_file_path = 'D:\myPython\SignalStrength\SignalStrengthResources\wifi_signal_strength.csv'
+csv_file_path = 'SignalStrength/SignalStrengthResources/wifi_signal_strength.csv'

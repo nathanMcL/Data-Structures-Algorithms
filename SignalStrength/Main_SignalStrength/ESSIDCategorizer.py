90772a62f01baeec2c3d310f83f8db467828a59f
Nathaniel Mclaughlin <Nmclaughlin296@gmail.com>
2023-12-07 16:03:25 +0000

    added pseudo and ESSIDCatogorizer code

---
+++ SignalStrength/Main_SignalStrength/ESSIDCategorizer.py
@@ -0,0 +1,63 @@
+# ESSID,Quality,Category
+# NetworkName,68/70,Excellent
+# NetworkName,52/70,Good
+# NetworkName,42/70,Fair
+# NetworkName,37/70,Weak
+# NetworkName,28/70,Poor
+
+class ESSIDCategorizer:
+    def __init__(self, data):
+        # Constructor: Initializes the ESSIDCategorizer with the provided data.
+        self.data = data
+
+    def categorize_essids(self):
+        # Initialize lists to hold ESSIDs categorized by signal strength.
+        poor_range = []
+        weak_range = []
+        fair_range = []
+        good_range = []
+        excellent_range = []
+
+        # Iterate through each row in the data.
+        for row in self.data:
+            try:
+                # Check if both 'ESSID' and 'Quality' columns exist in the row.
+                if 'ESSID' in row and 'Quality' in row:
+                    # Split the 'ESSID' value by '/' and check if the first part is numeric.
+                    quality_parts = row['ESSID'].split('/')
+                    if quality_parts[0].isnumeric():
+                        # Convert the first part of 'ESSID' to an integer (represents quality).
+                        quality = int(quality_parts[0])
+
+                        # Categorize the ESSID into different ranges based on quality value.
+                        if 0 <= quality <= 28:
+                            poor_range.append(row['ESSID'])
+                        elif 29 <= quality <= 37:
+                            weak_range.append(row['ESSID'])
+                        elif 38 <= quality <= 42:
+                            fair_range.append(row['ESSID'])
+                        elif 43 <= quality <= 52:
+                            good_range.append(row['ESSID'])
+                        elif 53 <= quality <= 68:
+                            excellent_range.append(row['ESSID'])
+                    else:
+                        # Print a warning if the quality value is not numeric.
+                        print(f"Non-numeric quality value in ESSID: {row['ESSID']}")
+                else:
+                    # Print a warning if either 'Quality' or 'ESSID' column is missing.
+                    print("Missing 'Quality' or 'ESSID' column in row:", row)
+            except Exception as e:
+                # Catch and print any other exceptions encountered.
+                print(f"Error processing row {row}: {e}")
+
+        # Return a dictionary containing sets of categorized ESSIDs.
+        # Sets are used to ensure uniqueness within each category.
+        return {
+            'Poor Range': set(poor_range),
+            'Weak Range': set(weak_range),
+            'Fair Range': set(fair_range),
+            'Good Range': set(good_range),
+            'Excellent Range': set(excellent_range)
+        }
+
+

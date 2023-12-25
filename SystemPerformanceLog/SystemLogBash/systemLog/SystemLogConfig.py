b5fef2cfe221293312301b7b3217239e4710ab75
Nathaniel Mclaughlin <Nmclaughlin296@gmail.com>
2023-12-25 20:39:52 +0000

    updated os log path

--- SystemPerformanceLog/SystemLogBash/systemLog/SystemLogConfig.py
+++ SystemPerformanceLog/SystemLogBash/systemLog/SystemLogConfig.py
@@ -1,2 +1,2 @@
 # Path to the CSV file
-os_log_path = '/home/macberrypi/Desktop/myPython/SystemPerformanceLog/SystemLogBash/SystemLogResources/os_log.csv'
+os_log_path = '/workspace/Data-Structures-Algorithms/SystemPerformanceLog/SystemLogBash/SystemLogResources/os_log.csv'
